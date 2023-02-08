e3a57cab811097f4f4c5a96530cf586cdd3a0eb0
davidRokach <davidrokach111@gmail.com>
2023-02-08 20:15:18 +0200

    git page workkk

--- src/app.js
+++ src/app.js
@@ -1,34 +1,34 @@
-//#region האזנה לאירועים
+// //#region האזנה לאירועים
 
-import PAGES from "./models/pageModel.js";
-import { onChangePage } from "./routes/router.js";
-import {
-  GALLERY_PAGE_LINK,
-  HOME_PAGE_LINK,
-  LAB_TEAM_PAGE_LINK,
-  PUBLICATIONS_PAGE_LINK,
-  RESEARCH_PAGE_LINK,
-  RESUME_PAGE_LINK,
-} from "./services/domServices.js";
+// import PAGES from "./models/pageModel.js";
+// import { onChangePage } from "./routes/router.js";
+// import {
+//   GALLERY_PAGE_LINK,
+//   HOME_PAGE_LINK,
+//   LAB_TEAM_PAGE_LINK,
+//   PUBLICATIONS_PAGE_LINK,
+//   RESEARCH_PAGE_LINK,
+//   RESUME_PAGE_LINK,
+// } from "./services/domServices.js";
 
-// ניתוב דפים
-HOME_PAGE_LINK.addEventListener("click", () => {
-  onChangePage(PAGES.HOME);
-});
-RESEARCH_PAGE_LINK.addEventListener("click", () => {
-  onChangePage(PAGES.RESEARCH);
-});
-PUBLICATIONS_PAGE_LINK.addEventListener("click", () => {
-  onChangePage(PAGES.PUBLICATIONS);
-});
-RESUME_PAGE_LINK.addEventListener("click", () => {
-  onChangePage(PAGES.RESUME);
-});
-LAB_TEAM_PAGE_LINK.addEventListener("click", () => {
-  onChangePage(PAGES.LAB_TEAM);
-});
-GALLERY_PAGE_LINK.addEventListener("click", () => {
-  onChangePage(PAGES.GALLERY);
-});
+// // ניתוב דפים
+// HOME_PAGE_LINK.addEventListener("click", () => {
+//   onChangePage(PAGES.HOME);
+// });
+// RESEARCH_PAGE_LINK.addEventListener("click", () => {
+//   onChangePage(PAGES.RESEARCH);
+// });
+// PUBLICATIONS_PAGE_LINK.addEventListener("click", () => {
+//   onChangePage(PAGES.PUBLICATIONS);
+// });
+// RESUME_PAGE_LINK.addEventListener("click", () => {
+//   onChangePage(PAGES.RESUME);
+// });
+// LAB_TEAM_PAGE_LINK.addEventListener("click", () => {
+//   onChangePage(PAGES.LAB_TEAM);
+// });
+// GALLERY_PAGE_LINK.addEventListener("click", () => {
+//   onChangePage(PAGES.GALLERY);
+// });
 
-//#endregion
+// //#endregion
